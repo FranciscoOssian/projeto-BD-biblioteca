fa35270ef4e1131ac7e7f081b774889f7c9054ad
Francsico Ossian <ossian.neto@gmail.com>
2025-02-14 19:51:06 -0300

    fix/models

--- database/services/book.py
+++ database/services/book.py
@@ -1,7 +1,8 @@
 from database.services.generic import GenericService
+from models.Book import Book
 
 class BookService(GenericService):
     def __init__(self, conn):
-        super().__init__(conn, "book")  # Chama o construtor da classe pai (GenericService) com o nome da entidade
+        super().__init__(conn, "book", Book)  # Chama o construtor da classe pai (GenericService) com o nome da entidade
         
     
