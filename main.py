c55031c9523d0d1c3f779bde93e1760af8e76797
Francsico Ossian <ossian.neto@gmail.com>
2025-02-15 18:03:54 -0300

    feature/db/triggers

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 import api.read.book
 import api.read.intern
 import api.read.student
+import os
+from database.utils import execute_triggers, get_db
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+conn = get_db()
+execute_triggers(conn, base_dir + '/database/queries/triggers')
 
 app = FastAPI(title="Mini Sistema de Biblioteca")
 
